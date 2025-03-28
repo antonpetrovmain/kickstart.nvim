d5bea25c7e41d93678e673ed152f4debf1f13f27
Anton Petrov <apetrov@eservicios.indracompany.com>
2025-03-28 15:31:49 +0200

    Add continue marker note :)

--- init.lua
+++ init.lua
@@ -214,6 +214,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- NOTE: (apetrov) Continue reading from here :)
+--
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
